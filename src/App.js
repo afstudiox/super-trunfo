b9709b3a1f139834b4a6856d089428987b524ebd
Andre Felipe <afelipes@gmail.com>
2022-02-23 19:14:04 -0300

    3. Crie e renderize o componente Card com as props necessárias

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 import './components/Form.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
       <div>
         <h1>Tryunfo</h1>
         <Form />
+        <Card />
       </div>
     );
   }
